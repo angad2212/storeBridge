dc51011ea23bb7faffc903c847211c9add3ea393
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2024-08-18 02:22:07 +0530

    setup

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.status(200).send({
+        message: 'welcome to the dataBridgr server', status:true
+    })
+})
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+const app = require('./index')
+PORT = 3000
+
+app.listen(PORT, ()=>{
+    console.log(`working on port ${PORT}`)
+})
